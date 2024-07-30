c35f02fc3adee0f57d547bffc1760a4b51e15cad
AldenKim <aldenkim.ut@gmail.com>
2024-07-30 13:30:19 -0600

    Make some progress with startGame function in EvilHangman

--- src/hangman/EvilHangman.java
+++ src/hangman/EvilHangman.java
@@ -1,9 +1,16 @@
 package hangman;
 
+import java.io.File;
+
 public class EvilHangman {
 
     public static void main(String[] args) {
-
+        EvilHangmanGame test = new EvilHangmanGame();
+        try {
+            test.startGame(new File(args[0]), Integer.parseInt(args[1]));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/hangman/EvilHangmanGame.java
+++ src/hangman/EvilHangmanGame.java
@@ -1,13 +1,33 @@
 package hangman;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.SortedSet;
 
 public class EvilHangmanGame implements IEvilHangmanGame {
+    Set<String> words = new HashSet<>();
     @Override
     public void startGame(File dictionary, int wordLength) throws IOException, EmptyDictionaryException {
+        if (dictionary == null) {
+            throw new IOException("Empty dictionary");
+        }
+
+        BufferedReader reader = new BufferedReader(new FileReader(dictionary));
+        String line = reader.readLine();
+
+        while (line != null) {
+            if(line.length() == wordLength) {
+                System.out.println(line);
+                words.add(line);
+            }
+            line = reader.readLine();
+        }
+        reader.close();
 
     }
 
