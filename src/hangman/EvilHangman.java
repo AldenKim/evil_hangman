9359ce5a0e3fc2e7319292805c19ccfac064717e
AldenKim <aldenkim.ut@gmail.com>
2024-07-30 14:38:07 -0600

    Add changes to startGame function to pass tests, add variables to help keep track of game state

--- src/hangman/EvilHangman.java
+++ src/hangman/EvilHangman.java
@@ -5,9 +5,10 @@ import java.io.File;
 public class EvilHangman {
 
     public static void main(String[] args) {
-        EvilHangmanGame test = new EvilHangmanGame();
+        EvilHangmanGame game = new EvilHangmanGame();
+        game.getGuess(Integer.parseInt(args[2]));
         try {
-            test.startGame(new File(args[0]), Integer.parseInt(args[1]));
+            game.startGame(new File(args[0]), Integer.parseInt(args[1]));
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/hangman/EvilHangmanGame.java
+++ src/hangman/EvilHangmanGame.java
@@ -4,31 +4,51 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedSet;
+import java.util.*;
 
 public class EvilHangmanGame implements IEvilHangmanGame {
-    Set<String> words = new HashSet<>();
+    private final Set<String> words = new HashSet<>();
+    private final Set<String> guesses = new HashSet<>();
+    private int guessCount;
+    private String currentPattern;
+
+    public void getGuess(int count) {
+        guessCount = count;
+    }
+
     @Override
     public void startGame(File dictionary, int wordLength) throws IOException, EmptyDictionaryException {
         if (dictionary == null) {
             throw new IOException("Empty dictionary");
         }
+        if(wordLength == 0) {
+            throw new EmptyDictionaryException();
+        }
 
         BufferedReader reader = new BufferedReader(new FileReader(dictionary));
         String line = reader.readLine();
 
+        if(Objects.equals(line, null)) {
+            throw new EmptyDictionaryException();
+        }
+
         while (line != null) {
             if(line.length() == wordLength) {
-                System.out.println(line);
                 words.add(line);
             }
             line = reader.readLine();
         }
         reader.close();
 
+        if(words.isEmpty()) {
+            throw new EmptyDictionaryException();
+        }
+
+        guesses.clear();
+        currentPattern = "";
+        for(int i = 0; i < wordLength; i++) {
+            currentPattern += "-";
+        }
     }
 
     @Override
